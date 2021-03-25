f750765ada56f710a71d5b3c042aa6814b252b70
markeezy <markkhoo95@gmail.com>
2021-03-24 17:12:32 -0700

    add password generation code

--- Assets/script.js
+++ Assets/script.js
@@ -13,42 +13,61 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Variables for characters
+var uChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lChars = "abcdefghijklmnopqrstuvwxyz";
+var nChars = "0123456789";
+var sChars = "~!@#$%^&*?";
+
 // generatePassword function code below
 function generatePassword() {
+  var actualPassword = "";
+  
   alert("You will be presented with the following prompts to select criteria for generating a password.");
 
+  // choice of password length between 8-128
   var pwdLength = prompt("Choose a number between 8 and 128. This choice will be the length of the password.", );
   if (pwdLength != null) {
     if (pwdLength >= 8 && pwdLength <= 128) {
 
+      // choice of Lowercase, Uppercase, Numerals, Special Character
       alert("In the following prompts, at least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
 
       var chooser = false;
+      var charChoices = "";
 
       var uCase = confirm("Would you like your password to contain uppercase characters?");
       if (uCase === true) {
         chooser = true;
+        charChoices += uChars; 
       }
 
       var lCase = confirm("Would you like your password to contain lowercase characters?");
       if (lCase === true) {
         chooser = true;
+        charChoices += lChars;
       }
 
       var numChoice = confirm("Would you like your password to contain numeric characters?");
       if (numChoice === true) {
         chooser = true;
+        charChoices += nChars;
       }
 
       var specChar = confirm("Would you like your password to contain special characters?");
       if (specChar === true) {
         chooser = true;
+        charChoices += sChars;
       }
 
       if (chooser === false) {
         alert("At least one choice of lowercase, uppercase, numeric, and/or special characters must be confirmed.");
       } else {
-        alert("TIME TO GENERATE PASSWORD LOL");
+
+        // actually password generation code
+        for (i = 1; i < pwdLength ; i++ ) {
+          actualPassword += charChoices[Math.floor(Math.random()*charChoices.length)];
+        };
       };
 
     } else {
@@ -58,6 +77,6 @@ function generatePassword() {
     alert("Entry is null. Please start over.");
   };
 
-  return "Length: " + pwdLength + " /// Uppercase: " + uCase + " /// Lowercase: " + lCase + " /// Numerals: " + numChoice + " /// Special Characters: " + specChar;
+  return actualPassword;
 }
 
